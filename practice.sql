ea6f18fcbfc81616ff23a8526f684d03d2578789
Katarina <klingat@me.com>
2016-07-18 19:34:52 +0000

    added exercise 2

---
+++ practice.sql
@@ -0,0 +1,10 @@
+INSERT INTO Account (email, password, createdOn, modifiedOn) VALUES 
+  ("klingt@me.com", "123456", "2016-06-10", "2016-06-18");
+  
+INSERT INTO AddressBook (name, createdOn, modifiedOn) VALUES 
+("Kasia", "2016-06-10", "2016-06-18");
+
+INSERT INTO Entry (firstName, lastName, birthday, type, subtype, contentLineOne, contentLineTwo, contentLineThree, contentLineFour, contentLineFive) VALUES 
+("Katarina", "Lingat", "1994-03-26", "home", "phone", "hello", "how", "are", "you", "?"),
+("Claudia", "Lingat", "1990-11-16", "work", "email", "dog", "cat", "mouse", "fish", "N/A"),
+("Katarina", "Lingat", "1955-03-05", "other", "address", "i", "see", "you", "N/A", "N/A");
